4ebcdf84642600449f48caf06094b5da9eb67d6c
Andrei Alekseichuk <alekanandy@hotmail.com>
2021-06-14 11:43:21 -0400

    Added heyNow method

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello from GitHub");
 
+	} 
+	public static void heyNow() {
+		String Msg = "Hey now!";
+		System.out.println(Msg);
 	}
 
 }
